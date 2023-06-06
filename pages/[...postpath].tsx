ef478a86671424c6a9338911482e23316fb3eff2
Muhammadbilal Taj <60062343+muhammadbilaltaj121@users.noreply.github.com>
2023-06-06 21:33:26 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
+					"https://usahealthtip.com/"+ encodeURI(path as string)
 				}`,
 			},
 		};
